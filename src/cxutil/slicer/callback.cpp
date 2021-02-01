48fa25fc7770ad14243069af20570547d88098d8
ZengGui <anoob@sina.cn>
2021-02-01 19:38:45 +0800

    refactor: 💡 callback

--- src/cxutil/slicer/callback.cpp
+++ src/cxutil/slicer/callback.cpp
@@ -2,5 +2,23 @@
 
 namespace cxutil
 {
+	void SliceCallback::onExtruderChanged(int index)
+	{
 
+	}
+
+	void SliceCallback::onTotalLayers(int num)
+	{
+
+	}
+
+	void SliceCallback::onWrite(float velocity, int x, int y, int z, PrintFeatureType type)
+	{
+
+	}
+
+	void SliceCallback::onLayerStart(int layer, float thickness)
+	{
+
+	}
 }
--- src/cxutil/slicer/callback.h
+++ src/cxutil/slicer/callback.h
@@ -1,6 +1,7 @@
 #ifndef CXUTIL_CALLBACK_1607502965747_H
 #define CXUTIL_CALLBACK_1607502965747_H
 #include "cxutil/math/AABB3D.h"
+#include "cxutil/settings/PrintFeature.h"
 
 namespace cxutil
 {
@@ -10,14 +11,20 @@ namespace cxutil
 	public:
 		virtual ~SliceCallback() {}
 
-		virtual void onSceneBox(const AABB3D& box3) = 0;
-		virtual void onLayerCount(int layer) = 0;
-		virtual void onFilamentLen(double len) = 0;
-		virtual void onPrintTime(int time) = 0;
+		virtual void onSceneBox(const AABB3D& box3) {};
+		virtual void onLayerCount(int layer) {};
+		virtual void onFilamentLen(double len) {};
+		virtual void onPrintTime(int time) {};
 
-		virtual void onLayerPart(int meshIdx, int layerIdx, int partIdx, float z, float thickness, ClipperLib::Path& path) = 0;
-		virtual void onSupport(int layerIdx, int partIdx, float thickness, ClipperLib::Path& path) = 0;
-		virtual void onCxutilMesh(Mesh* mesh) = 0;
+		virtual void onLayerPart(int meshIdx, int layerIdx, int partIdx, float z, float thickness, ClipperLib::Path& path) {};
+		virtual void onSupport(int layerIdx, int partIdx, float thickness, ClipperLib::Path& path) {};
+		virtual void onCxutilMesh(Mesh* mesh) {};
+
+		//GCode
+		virtual void onExtruderChanged(int index);
+		virtual void onTotalLayers(int num);
+		virtual void onWrite(float velocity, int x, int y, int z, PrintFeatureType type);
+		virtual void onLayerStart(int layer, float thickness);
 	};
 }
 
