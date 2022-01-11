a9f8b9c1dec044e4e3f655ac148e0a11ec5fb49f
Yi Chenlin <yichenlin@creality.com>
2022-01-11 14:09:17 +0800

    z seam
    Change-Id: Ic2bdc41e9d18c2fb1839d755451db01e5075d3d9

--- cxutil/settings/Settings.cpp
+++ cxutil/settings/Settings.cpp
@@ -437,6 +437,10 @@ namespace cxutil
         {
             return EZSeamType::USER_SPECIFIED;
         }
+        else if (value == "experimental")
+        {
+            return EZSeamType::EXPERIMENTAL;
+        }
         else //Default.
         {
             return EZSeamType::SHORTEST;
