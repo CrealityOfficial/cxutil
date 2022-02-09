163d2712b36c4ee3e0d948f66f771c95d4dd656f
baijunjiang <baijunjiang@creality.com>
2022-02-09 18:31:34 +0800

    添加所有的参数
    Change-Id: Icf08f3e579af074944eefd545b7f9a0858a4b503

--- cxutil/input/dlpinput.h
+++ cxutil/input/dlpinput.h
@@ -8,6 +8,7 @@ namespace trimesh {
 
 namespace cxutil
 {
+	
 	struct DLPParam
 	{
 		coord_t initial_layer_thickness;
@@ -17,6 +18,17 @@ namespace cxutil
 		coord_t line_segment_resolution;
 		coord_t line_segment_deviation;
 
+		float xy_offset;
+		bool enable_xy_offset;
+
+		bool enable_b_offset;
+		float sThickness;
+		float sh;
+		float sLength;
+		float sWidth;
+		float pixX;
+		float pixY;
+
 		DLPParam()
 		{
 			initial_layer_thickness = 300;
@@ -25,8 +37,20 @@ namespace cxutil
 			minimum_polygon_circumference = 1000;
 			line_segment_resolution = 50;
 			line_segment_deviation = 50;
+
+			xy_offset = 0;
+			enable_xy_offset = false;
+
+			enable_b_offset = false;
+			sThickness = 0.5 * 1000;
+			sh = 100 * 1000;
+			sLength = 192 * 1000;
+			sWidth = 1000;
+			pixX = 3240;
+			pixY = 2400;
 		}
 	};
+
 
 	class DLPInput
 	{
