c07fc912003f4505f70861cdfdb2298af3b0213f
zenggui <anoob@sina.cn>
2023-09-10 09:29:59 +0800

    feature:[] remove crcommon

--- cxutil/math/polygon.cpp
+++ cxutil/math/polygon.cpp
@@ -10,7 +10,38 @@
 #include <sstream>
 #include <iomanip>
 
-#include "crcommon/header.h"
+template<class T>
+void templateSave(const T& t, std::ofstream& out)
+{
+    out.write((const char*)&t, sizeof(T));
+}
+
+template<class T>
+T templateLoad(std::ifstream& in)
+{
+    T t;
+    in.read((char*)&t, sizeof(T));
+    return t;
+}
+
+template<class T>
+void templateSave(const std::vector<T>& ts, std::ofstream& out)
+{
+    int size = (int)ts.size();
+    templateSave<int>(size, out);
+    out.write((const char*)ts.data(), size * sizeof(T));
+}
+
+template<class T>
+void templateLoad(std::vector<T>& ts, std::ifstream& in)
+{
+    int size = templateLoad<int>(in);
+    if (size > 0)
+    {
+        ts.resize(size);
+        in.read((char*)ts.data(), size * sizeof(T));
+    }
+}
 
 namespace cxutil
 {
